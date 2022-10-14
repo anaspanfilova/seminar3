efab0db3cf424f4016da0603e383edd682569921
Анастасия <understooooood@mail.ru>
2022-10-14 14:52:29 +0300

    решили 22 задачу

---
+++ 22/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+void Kvadrat(int a)
+{
+    for (int i = 1; i <= a; i++) 
+    {
+    Console.Write($"{i*i},");
+    }
+}
+Console.Clear();
+Console.Write("Введите число: ");
+int x = int.Parse(Console.ReadLine()!);
+Kvadrat(x);
